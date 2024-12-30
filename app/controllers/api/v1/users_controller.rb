e75bc6a0b49e5c8ac812136f37a33a94545c265f
magicmarie <natukunda162@gmail.com>
2024-12-30 11:08:26 -0600

    Add user model

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
